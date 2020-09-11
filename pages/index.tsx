0f6ae29adc3625bf42dedff50a73b66247ef0cd5
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2020-09-12 07:54:27 +0900

    Add a normal speed page with some components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,35 @@
 import {
+  Box,
+  Button,
   Container,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
+  Heading,
+  Link,
+  useToast,
 } from '@chakra-ui/core'
 import React from 'react'
 
 export default function Home() {
+  const toast = useToast()
   return (
     <Container my={6}>
-      <FormControl mt={4}>
-        <FormLabel>Email address</FormLabel>
-        <Input type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel>Email address1</FormLabel>
-        <Input type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel>Email address2</FormLabel>
-        <Input type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel>Email address3</FormLabel>
-        <Input type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel>Email address4</FormLabel>
-        <Input type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel>Email address5</FormLabel>
-        <Input type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
+      <Heading>Hello</Heading>
+      <Box>This page should be loaded</Box>
+      <Box mt={4}>
+        <Link href="/slow" textDecor="underline">
+          And this will be slow
+        </Link>
+      </Box>
+      <Box mt={4}>
+        <Button
+          borderRadius="full"
+          variant="outline"
+          onClick={() => {
+            toast({ title: 'Hello', description: 'demo', status: 'success' })
+          }}
+        >
+          Button Will be Fine
+        </Button>
+      </Box>
     </Container>
   )
 }
